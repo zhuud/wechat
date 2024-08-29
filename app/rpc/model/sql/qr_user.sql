eac774ad80885067d9a4649cf3007ca71ae0d45c
jinjiangtao <jinjiangtao@soyoung.com>
2024-08-29 11:48:53 +0800

    update code

--- app/rpc/model/sql/qr_user.sql
+++ app/rpc/model/sql/qr_user.sql
@@ -1,5 +1,5 @@
-CREATE TABLE user_qr (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE user_service_qrcode (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
     `config_id` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '新增联系方式的配置id',
     `type` INT NOT NULL DEFAULT 1 COMMENT '联系方式类型，1-单人，2-多人',
     `scene` INT NOT NULL DEFAULT 1 COMMENT '场景，1-在小程序中联系，2-通过二维码联系',
@@ -10,7 +10,7 @@ CREATE TABLE user_qr (
     `qr_code` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '联系二维码的URL，仅在scene为2时返回',
     `user` VARCHAR(255) NOT NULL DEFAULT 'zhangsan,lisi,wangwu' COMMENT '使用该联系方式的用户userID列表',
     `party` VARCHAR(255) NOT NULL DEFAULT '2,3' COMMENT '使用该联系方式的部门id列表',
-    `is_temp` BOOLEAN NOT NULL DEFAULT true COMMENT '是否临时会话模式',
+    `is_temp` tinyint(1) NOT NULL DEFAULT true COMMENT '是否临时会话模式0 不是 1 是',
     `expires_in` INT NOT NULL DEFAULT 86400 COMMENT '临时会话二维码有效期，以秒为单位',
     `chat_expires_in` INT NOT NULL DEFAULT 86400 COMMENT '临时会话有效期，以秒为单位',
     `unionid` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '可进行临时会话的客户unionid',
@@ -18,5 +18,5 @@ CREATE TABLE user_qr (
     `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间 | 2020-09-10',
     `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间 | 2020-09-10',
     PRIMARY KEY (`id`)
-);
+)ENGINE = InnoDB COMMENT = '企业微信客服二维码信息| 2020-09-10';;
 
