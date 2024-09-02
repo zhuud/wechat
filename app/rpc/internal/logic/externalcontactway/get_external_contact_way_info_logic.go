8df67c336d8818174cc6c26bdc3e6a94100b781b
jinjiangtao <jinjiangtao@soyoung.com>
2024-09-02 11:05:36 +0800

    add 添加字段

--- app/rpc/internal/logic/externalcontactway/get_external_contact_way_info_logic.go
+++ app/rpc/internal/logic/externalcontactway/get_external_contact_way_info_logic.go
@@ -9,12 +9,6 @@ import (
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
-var svcCtx *svc.ServiceContext
-
-func init() {
-	svcCtx = svc.NewServiceContext(config.MustLoad())
-}
-
 type GetExternalContactWayInfoLogic struct {
 	ctx    context.Context
 	svcCtx *svc.ServiceContext
@@ -32,17 +26,26 @@ func NewGetExternalContactWayInfoLogic(ctx context.Context, svcCtx *svc.ServiceC
 func (l *GetExternalContactWayInfoLogic) GetExternalContactWayInfo(in *wechat.ExternalContactWayReq) (*wechat.ExternalContactWayInfoResp, error) {
 	// todo: add your logic here and delete this line
 
-	configData, err := svcCtx.WeCom.WithCorp("yx").ContactWay.Get(context.Background(), in.ConfigId)
-	
+	configData, err := l.svcCtx.WeCom.WithCorp("yx").ContactWay.Get(context.Background(), in.ConfigId)
+
 	if err != nil {
 		return nil, err
 	}
 
 	return &wechat.ExternalContactWayInfoResp{
 		ContactWay: &wechat.ExternalContactWayData{
-			ConfigId: configData.ContactWay.ConfigID,
-			QrCode: configData.ContactWay.QrCode,
-			
+			ConfigId:      configData.ContactWay.ConfigID,
+			QrCode:        configData.ContactWay.QrCode,
+			Type:          int32(configData.ContactWay.Type),
+			Scene:         int32(configData.ContactWay.Scene),
+			User:          configData.ContactWay.User,
+			IsTemp:        configData.ContactWay.IsTemp,
+			ExpiresIn:     int32(configData.ContactWay.ExpiresIn),
+			ChatExpiresIn: int32(configData.ContactWay.ChatExpiresIn),
+			Unionid:       configData.ContactWay.UnionID,
+			Conclusions:   &wechat.ExternalContactWayConclusion{
+
+			},
 		},
 	}, nil
 }
