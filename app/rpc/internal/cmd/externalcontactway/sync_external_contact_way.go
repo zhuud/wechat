d4b922ebb8ba3b74e68f97981dbbcf35e262dbe2
jinjiangtao <jinjiangtao@soyoung.com>
2024-10-08 14:42:49 +0800

    update add 字段

--- app/rpc/internal/cmd/externalcontactway/sync_external_contact_way.go
+++ app/rpc/internal/cmd/externalcontactway/sync_external_contact_way.go
@@ -2,7 +2,10 @@ package externalcontactway
 
 import (
 	"context"
+	"encoding/json"
+	"time"
 
+	"rpc/internal/config"
 	"rpc/internal/svc"
 	"rpc/model"
 
@@ -54,6 +57,39 @@ func (s *syncExternalContactWayCmd) Do(args []string) error {
 				s.Error(err)
 				continue
 			}
+			/**
+					Id            uint64    `db:"id"`              // 主键id
+			ConfigId      string    `db:"config_id"`       // 新增联系方式的配置id
+			Type          int64     `db:"type"`            // 联系方式类型，1-单人，2-多人
+			Scene         int64     `db:"scene"`           // 场景，1-在小程序中联系，2-通过二维码联系
+			Style         int64     `db:"style"`           // 小程序中联系按钮的样式，仅在scene为1时返回，详见附录
+			Remark        string    `db:"remark"`          // 联系方式的备注信息，用于助记
+			SkipVerify    int64     `db:"skip_verify"`     // 外部客户添加时是否无需验证 0-否 1-是
+			State         string    `db:"state"`           // 企业自定义的state参数，用于区分不同的添加渠道
+			QrCode        string    `db:"qr_code"`         // 联系二维码的URL，仅在scene为2时返回
+			User          string    `db:"user"`            // 使用该联系方式的用户userID列表
+			Party         string    `db:"party"`           // 使用该联系方式的部门id列表
+			IsTemp        int64     `db:"is_temp"`         // 是否临时会话模式0 不是 1 是
+			ExpiresIn     int64     `db:"expires_in"`      // 临时会话二维码有效期，以秒为单位
+			ChatExpiresIn int64     `db:"chat_expires_in"` // 临时会话有效期，以秒为单位
+			Unionid       string    `db:"unionid"`         // 可进行临时会话的客户unionid
+			IsExclusive   int64     `db:"is_exclusive"`    // 0-否 1-是；是否开启同一外部企业客户只能添加同一个员工**/
+
+			var skipVerifyIntValeue int64
+			if configData.ContactWay.SkipVerify {
+				skipVerifyIntValeue = 1
+			}
+
+			var isTemp int64
+			if configData.ContactWay.IsTemp {
+				isTemp = 1
+			}
+
+			var isExclusive int64
+			
+			userList,_ := json.Marshal(configData.ContactWay.User)
+			partyList,_ := json.Marshal(configData.ContactWay.Party)
+
 			s.svcCtx.ModelUserServiceQrcodeModel.Insert(s.ctx, &model.UserServiceQrcode{
 				ConfigId:  configId,
 				Type:      int64(configData.ContactWay.Type),
@@ -63,6 +99,15 @@ func (s *syncExternalContactWayCmd) Do(args []string) error {
 				Remark:    configData.ContactWay.Remark,
 				Style:     int64(configData.ContactWay.Style),
 				ExpiresIn: int64(configData.ContactWay.ExpiresIn),
+				SkipVerify: skipVerifyIntValeue,
+				User: string(userList),
+				Party: string(partyList),
+				IsTemp: isTemp,
+				ChatExpiresIn: int64(configData.ContactWay.ChatExpiresIn),
+				Unionid: configData.ContactWay.UnionID,
+				IsExclusive: isExclusive,
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
 			})
 		}
 
